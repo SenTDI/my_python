6edab066c1d390b3aca425fa1f1b02bb249faf2b
SenTDI <47959600+SenTDI@users.noreply.github.com>
2020-05-22 18:38:34 +0500

    Debug
    FastFix Error

--- ypr.py
+++ ypr.py
@@ -1,9 +1,9 @@
 message = ["Тебе нужно быть сегодня в 9 дома !","На столе лежат яблоки."]
 sent_message = []
 def send_message(message,sent_message):
-                 while message:
-                               message_1 = message.pop()
-                               sent_message.append(message_1)
-                  print(message)
-                  print(sent_message)
+  while message:
+    message_1 = message.pop()
+    sent_message.append(message_1)
 send_message(message[:],sent_message)
+print(message)
+print(sent_message)
